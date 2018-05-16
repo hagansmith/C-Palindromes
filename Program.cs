b9edf6c3db1f34fda6a1a43283bfc693c58c02ed
Adam Smith <hagansmith@gmail.com>
2018-05-15 20:39:51 -0500

    solution

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Palindromes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var forward = input.ToLower().Replace(" ", "");
+            var cleanforwardCharacters = Regex.Replace(forward, "(?:[^a-z0-9])", "");
+            Console.WriteLine(cleanforwardCharacters);
+            var reverse = cleanforwardCharacters.ToCharArray();
+            
+            Array.Reverse(reverse);
+            string s = new string(reverse);
+            Console.WriteLine(reverse);
+            
+            Console.WriteLine(cleanforwardCharacters == s ? "true" : "false");
+        }
+    }
+}
